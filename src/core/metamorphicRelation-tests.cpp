5d4e0d775f1653e09e9617a561a1020919e42f2c
KernalPanik <lukas.michnevic@protonmail.com>
2025-03-04 22:10:56 +0200

    transforms begin to be applied

--- src/core/metamorphicRelation-tests.cpp
+++ src/core/metamorphicRelation-tests.cpp
@@ -2,6 +2,7 @@
 #include "../../test/Framework/testRunnerUtils.h"
 #include "src/Common/tuple-utils.h"
 #include "mrSearch.h"
+#include "Modules/Callable/transformer.h"
 
 #include <iostream>
 #include <cmath>
@@ -20,6 +21,27 @@ overload to use. Although this is a design issue, It's easy to mitigate via slim
 double poww(double x, double y) {
     return pow(x, y);
 }
+
+void Add(double& b, double val) {
+    b += val;
+}
+
+void Mul(double& b, double val) {
+    b *= val;
+}
+
+void Sub(double& b, double val) {
+     b -= val;
+}
+
+void Div(double& b, double val) {
+    b /= val;
+}
+
+#pragma endregion
+
+#pragma region transformers
+
 #pragma endregion
 
 #pragma region tmpMRContext
@@ -42,39 +64,46 @@ struct MRContext {
 // Straightforward Generation of MRs without using MR generation function
 void MR_SimpleConstructionTest() {
     // MR: pow(x, y+1) == pow(x, y) * x
-/*
-    auto func = ConstructTestableFunction<double, double&, double&>(poww);
-    std::vector<TupleWrapperBase> packedInputs;
-    
     // TODO: template util for packed input creation
     // BuildPackedInputs(std::vector<T> args)
-    std::vector<double> baseInputs = {static_cast<double>(10.0f), static_cast<double>(11.0f), static_cast<double>(12.0f), static_cast<double>(13.0f)};
-    std::vector<double> expInputs = {static_cast<double>(2.0f), static_cast<double>(2.0f), static_cast<double>(4.0f)};
+
+    std::vector<std::shared_ptr<ITestContext>> contexts;
+    std::vector baseInputs = {static_cast<double>(10.0f), static_cast<double>(11.0f), static_cast<double>(12.0f), static_cast<double>(13.0f)};
+    std::vector expInputs = {static_cast<double>(2.0f), static_cast<double>(3.0f), static_cast<double>(4.0f)};
     for (auto &b : baseInputs) {
         for (auto &e : expInputs) {
-            auto packedInput = TupleWrapper(static_cast<double>(b), static_cast<double>(e));
-            packedInputs.push_back(packedInput);
+            contexts.push_back(std::make_shared<TestContext<double, double, double>>(poww, b, e));
         }
     }
-*/
-#pragma region MRGen
-/*    std::vector<double> initialStates;
-    for (auto &i : packedInputs) {
-        //auto state = InvokeWithPackedArguments<double>(func, std::move(i));
+
+    for (auto &ctx : contexts) {
+        ctx->PrintState();
+        ctx->TestInvoke();
+        ctx->PrintState();
+        std::cout << std::endl;
     }
-/*
-    packedInputs.push_back(packedInput);
 
-    auto ctx = MRContext(func, packedInputs);    
-    //using fType = std::invoke_result_t<decltype(poww), double, double>;
+#pragma region TransformerPrep
 
-    // MR context generator still needs to get function return type T...
-    auto finalState = InvokeWithPackedArguments<double>(func, std::move(packedInput.tup));
+    std::vector<double> transformArgsForDiv = {1.0f, 2.0f, -1.0f};
+    std::vector<double> transformArgsGeneric = {1.0f, 2.0f, -1.0f, 0.0f, -2.0f};
 
-    std::cout << TupleToString(packedInput.tup) << std::endl;
-    std::cout << TupleToString(finalState) << std::endl;
-    std::cout << TupleToString(packedInput.tup) << std::endl;
-*/
+    auto DivPool = TransformPool<double, double>(Div, transformArgsForDiv);
+    auto divTransforms = DivPool.GetTransformers(0);
+
+    for (auto &ctx : contexts) {
+        for (auto &t : divTransforms) {
+            ctx->ValidateTransformChains(t);
+        }
+        std::cout << std::endl;
+    }
+
+#pragma endregion
+
+#pragma region MRGen
+
+
+
 #pragma endregion
     TEST_EXPECT(2 == 2);
 }

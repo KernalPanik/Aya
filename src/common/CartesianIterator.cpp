d7b0f4e20198941749a81985837a69f5ea8dd1c6
KernalPanik <lukas.michnevic@protonmail.com>
2025-03-09 21:56:30 +0200

    cartesian iterator

--- src/common/CartesianIterator.cpp
+++ src/common/CartesianIterator.cpp
@@ -10,20 +10,27 @@ void CartesianIterator::next() {
     const size_t lastIndex = m_Lengths.size() - 1;
     bool carry = false;
 
-    for (size_t index = lastIndex; index > 0; index--) {
-        if (carry) {
-            if (m_Indices[index] == m_Lengths[index] - 1) {
-                m_Indices[index] = 0;
-                carry = true;
-            } else {
-                m_Indices[index]++;
-            }
-        }
-        if (m_Indices[index] == m_Lengths[index] - 1) {
-            m_Indices[index] = 0;
-            carry = true;
-        } else {
+    m_Indices[lastIndex]++;
+    if (m_Indices[lastIndex] == m_Lengths[lastIndex]) {
+        carry = true;
+        m_Indices[lastIndex] = 0;
+    }
+
+    while (carry) {
+        for (int index = static_cast<int>(lastIndex)-1; index >= 0; index--) {
             m_Indices[index]++;
+            if (m_Indices[index] == m_Lengths[index]) {
+                carry = true;
+                m_Indices[index] = 0;
+                if (index == 0) {
+                    // still have carry? reset all to 0 since we went full circle
+                    carry = false;
+                    break;
+                }
+            } else {
+                carry = false;
+                break;
+            }
         }
     }
 }

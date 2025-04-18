f4150abef801277ed286c2f55d6a915d8aee8dd3
KernalPanik <lukas.michnevic@protonmail.com>
2025-04-18 21:28:53 +0300

    get file via filesystem

--- Aya/test/AyaSamples/main.cpp
+++ Aya/test/AyaSamples/main.cpp
@@ -2,7 +2,7 @@
 #include "PowTest.hpp"
 
 int main() {
-    GenerateMRsForSineOrCos(sine, "/Users/lukemichniewicz/source/repos/Aya/Aya/SineTest.txt", 1, 3);
+    GenerateMRsForSineOrCos(sine, std::filesystem::current_path().generic_string() + "/SineTest.txt", 1, 3);
     GenerateMRsForPow();
 
     return 0;

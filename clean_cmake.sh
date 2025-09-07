1f81243556f62f81667cc52b9c17ceddf15b9e85
KernalPanik <lukas.michnevic@protonmail.com>
2025-09-07 17:05:33 +0300

    fix that awkward bug

--- clean_cmake.sh
+++ clean_cmake.sh
@@ -3,5 +3,5 @@
 rm -rf CMakeFiles CMakeCache.txt Makefile cmake_install.cmake *.dir
 
 # Recursively remove CMake-generated files in src/samples subdirectories
-find ./src/samples -type f \( -name 'CMakeLists.txt' -o -name 'CMakeCache.txt' -o -name 'Makefile' -o -name 'cmake_install.cmake' \) -exec rm -f {} +
+find ./src/samples -type f \( -name 'CMakeCache.txt' -o -name 'Makefile' -o -name 'cmake_install.cmake' \) -exec rm -f {} +
 find ./src/samples -type d \( -name 'CMakeFiles' -o -name '*.dir' \) -exec rm -rf {} +

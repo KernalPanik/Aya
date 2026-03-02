97f050555682aea1591bdc40eab9ccc71c586991
ControlledMindFlux <uselessmachineai@gmail.com>
2026-03-02 20:39:27 +0200

    wipe old code

--- clean_cmake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Remove CMake-generated files and directories in the root
-rm -rf CMakeFiles CMakeCache.txt Makefile cmake_install.cmake *.dir
-
-# Recursively remove CMake-generated files in src/samples subdirectories
-find ./src/samples -type f \( -name 'CMakeCache.txt' -o -name 'Makefile' -o -name 'cmake_install.cmake' \) -exec rm -f {} +
-find ./src/samples -type d \( -name 'CMakeFiles' -o -name '*.dir' \) -exec rm -rf {} +
--- src/samples/stl/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "StlTests.hpp"
-
-int main(void){
-    MetamorphicRelationTest_VectorSize("VectorTest.txt");
-    return 0;
-}
